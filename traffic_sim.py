1dad1751bf6c9054f12646ecd80d5fdfc499adbd
BarnetteME1 <BarnetteME1@gmail.com>
2015-10-02 20:49:14 -0400

    got basic scatter plot working, but need to redo things

--- traffic_sim.py
+++ traffic_sim.py
@@ -3,18 +3,21 @@ import numpy as np
 
 class Car():
 
-    def __init__(self, max_kph = 120, current_speed = 0, length = 5,
-                start_location = 0, in_front = None):
+    def __init__(self, max_kph = 120, current_speed = 0, car_length = 5,
+                start_location = 0, in_front = None, course_length = 1000):
         self.max_kph = max_kph
         self.current_speed = current_speed
-        self.length = length
+        self.car_length = car_length
         self.location = start_location
         self.in_front = in_front
+        self.course_length = course_length
 
     @property
     def move_forward(self):
         self.change_velocity()
         self.location += self.current_speed
+        if self.location > self.course_length:
+            self.location = self.course_length % 1000
 
     @property
     def translate_max(self):
@@ -34,19 +37,27 @@ class Car():
             self.current_speed -= 2
 
     def change_velocity(self):
+        if self.distance_check():
+            self.keep_distance()
         if self.can_decelerate():
             self.decelerate()
         else:
             self.accelerate()
 
-    def keep_distance(self):
+    def distance_check(self):
         if self.in_front.location <= self.location + self.current_speed + 6:
-            self.current_speed = self.in_front.current_speed
+            return True
+        return False
+
+    def keep_distance(self):
+        self.current_speed = self.in_front.current_speed
 
 class Road():
 
     def __init__(self, road_length = 1000):
         self.road_length = road_length
+
+class Simulation:
 
 class Simulation:
 
@@ -57,14 +68,14 @@ class Simulation:
 
     def create_cars(self):
         cars = []
-        locations = np.linspace(0, self.length - 33, self.num_cars)
+        locations = np.linspace(0, self.length - 33, self.num_cars)[::-1]
+        old_car = None
         for num in range(self.num_cars):
-            if num == 0:
-                old_car = None
             new_car = Car(start_location = locations[num],
                           in_front = old_car)
             cars.append(new_car)
             old_car = new_car
+            cars[0].in_front = cars[-1]
         return cars
 
     def run_simulation(self):
@@ -73,7 +84,7 @@ class Simulation:
 
     def simulate(self):
         for car in self.cars:
-            new_car.move_forward()
+            car.move_forward
 
     def report(self):
         return [car.location for car in self.cars]
